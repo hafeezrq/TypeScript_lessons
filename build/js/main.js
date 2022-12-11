037d3fb7750822309ab6ed6cec2a32b5b6189029
hafeezrq <hafeezrq@yahoo.com>
2022-12-11 21:44:35 +1100

    Demonstrate basic types in TS

--- build/js/main.js
+++ build/js/main.js
@@ -1,3 +1,26 @@
-'use strict';
-let username = 'Zuhair';
-console.log(username);
+"use strict";
+/*
+ * Good to be explicit over implicit
+ */
+let firstName = 'Hafeez'; // TS infered type or called implicit type
+let lastName = 'Rehman'; // Explicity type
+let meaningOfLife;
+let isLoading;
+let album;
+let album1; // Union/composite type
+let re; // of Regular Expression type
+// meaningOfLife = '1999'; // Ok for JavaScript but not TS
+// if --noEmitError flag is on orr set in tsConfig.json
+// then it will not compile into js file otherwise it wiil
+// program will work normally.
+// follwing are right as album is of type 'any'
+album = 23;
+album = 'Van H';
+album = true;
+album1 = 1984; // OK
+album1 = 'Van Hellen'; //OK
+re = /\w+/g;
+const sume = (a, b) => {
+    // Type of 'a' and 'b' must be explicit otherwise TS not happy.
+    return a + b;
+};
--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,30 @@
-let userName = 'Zuhair';
-console.log(userName);
+/*
+ * Good to be explicit over implicit
+ */
+let firstName = 'Hafeez'; // TS infered type or called implicit type
+let lastName: string = 'Rehman'; // Explicity type
+let meaningOfLife: number;
+let isLoading: boolean;
+let album: any;
+let album1: string | number; // Union/composite type
+let re: RegExp; // of Regular Expression type
+
+// meaningOfLife = '1999'; // Ok for JavaScript but not TS
+// if --noEmitError flag is on orr set in tsConfig.json
+// then it will not compile into js file otherwise it wiil
+// program will work normally.
+
+// follwing are right as album is of type 'any'
+album = 23;
+album = 'Van H';
+album = true;
+
+album1 = 1984; // OK
+album1 = 'Van Hellen'; //OK
+
+re = /\w+/g;
+
+const sume = (a: number, b: number) => {
+  // Type of 'a' and 'b' must be explicit otherwise TS not happy.
+  return a + b;
+};
