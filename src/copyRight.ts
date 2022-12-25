155bee78930b32f2689fb31d08c08ebdd591c216
hafeezrq <hafeezrq@yahoo.com>
2022-12-25 21:58:51 +1100

    Real life example of assertion, also some styles.

---
+++ src/copyRight.ts
@@ -0,0 +1,25 @@
+// Vanilla JS code
+/* const year = document.getElementById('year');
+const thisYear = new Date().getFullYear();
+year.setAttribute("datetime", thisYear);
+year.textContent = thisYear; */
+// TS gives error on the above original JS code
+
+// Solution 1:
+/* let year: HTMLElement | null;
+year = document.getElementById('year')!;
+let thisYear: string;
+thisYear = new Date().getFullYear().toString();
+if(year) {
+  year.setAttribute('datetime', thisYear);
+  year.textContent = thisYear; 
+}
+*/
+
+// Solution 2:
+const year = document.getElementById('year') as HTMLSpanElement;
+const thisYear: string = new Date().getFullYear().toString();
+year.setAttribute('datetime', thisYear);
+year.textContent = thisYear;
+
+// The above code demonstrate the real world use of assertions.
