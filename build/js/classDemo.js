5c3518dd7a84b7a015274304b07170f5912d3523
hafeezrq <hafeezrq@yahoo.com>
2022-12-26 16:14:31 +1100

    Add file classDemo to some baic ways to declare the class.

---
+++ build/js/classDemo.js
@@ -0,0 +1,9 @@
+"use strict";
+class Coder {
+    constructor(name, music, age, lang) {
+        this.name = name; // must have the property, like name in this case.
+        this.music = music;
+        this.age = age;
+        this.lang = lang;
+    }
+}
---
+++ src/classDemo.ts
@@ -0,0 +1,13 @@
+class Coder {
+  name: string;
+  music: string;
+  age: number;
+  lang: string;
+
+  constructor(name: string, music: string, age: number, lang: string) {
+    this.name = name; // must have the property, like name in this case.
+    this.music = music;
+    this.age = age;
+    this.lang = lang;
+  }
+}
