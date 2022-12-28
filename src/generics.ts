0cc3dc1938dc680b0642a122bedfba7945f68ec0
hafeezrq <hafeezrq@yahoo.com>
2022-12-28 17:22:58 +1100

    Use generics in function and with keyof.

--- src/generics.ts
+++ src/generics.ts
@@ -15,3 +15,29 @@ console.log(isObject('John'));
 console.log(isObject([1, 2, 3]));
 console.log(isObject({ name: 'John' }));
 console.log(isObject(null));
+
+// Use with keyof assertion
+const isTrue = <T>(arg: T): { arg: T; is: boolean } => {
+  if (Array.isArray(arg) && !arg.length) {
+    return { arg, is: false };
+  }
+  if (isObject(arg) && !Object.keys(arg as keyof T).length) {
+    return { arg, is: false };
+  }
+  return { arg, is: !!arg };
+};
+
+console.log(isTrue(false));
+console.log(isTrue(0));
+console.log(isTrue(true));
+console.log(isTrue(1));
+console.log(isTrue('Dave'));
+console.log(isTrue(''));
+console.log(isTrue(null));
+console.log(isTrue(undefined));
+console.log(isTrue({}));
+console.log(isTrue({ name: 'Dave' }));
+console.log(isTrue([]));
+console.log(isTrue([1, 2, 3]));
+console.log(isTrue(NaN));
+console.log(isTrue(-0));
