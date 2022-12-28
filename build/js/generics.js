772cbbed7e25ae8fbb7163a9913a44f4c37fb5e3
hafeezrq <hafeezrq@yahoo.com>
2022-12-28 17:13:24 +1100

    Demo basics use of generics in TS.

---
+++ build/js/generics.js
@@ -0,0 +1,15 @@
+"use strict";
+// Generics: To define strict types for typesafe developer experience
+// Generics provide a placeholder, a type of variable
+// Example
+// Following function will eccept any type of parameter.
+const echo = (param) => param;
+// Dosn't have to be return type!
+const isObject = (arg) => {
+    return typeof arg === 'object' && !Array.isArray(arg) && arg !== null;
+};
+console.log(isObject(true));
+console.log(isObject('John'));
+console.log(isObject([1, 2, 3]));
+console.log(isObject({ name: 'John' }));
+console.log(isObject(null));
