daa5c2c0dca7a43d142e0097a9c7d27f2ecfdcb1
hafeezrq <hafeezrq@yahoo.com>
2022-12-28 19:16:00 +1100

    Use generic to avoid assertion keyof.

--- build/js/generics.js
+++ build/js/generics.js
@@ -53,3 +53,58 @@ const processUser = (user) => {
 console.log(processUser({ id: 1, name: 'Dave' }));
 console.log(processUser({ id: 1 }));
 // console.log(processUser({ name: 'Dave' })); // Without id gives error.
+// A little more complex example
+const getUserProperty = (users, key) => {
+    return users.map(user => user[key]); // we don't use assertion here
+    // and still it works fine because of 'K extends keyof T', i.e use of generic.
+};
+const usersArray = [
+    {
+        id: 1,
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email: 'Sincere@april.biz',
+        address: {
+            street: 'Kulas Light',
+            suite: 'Apt. 556',
+            city: 'Gwenborough',
+            zipcode: '92998-3874',
+            geo: {
+                lat: '-37.3159',
+                lng: '81.1496',
+            },
+        },
+        phone: '1-770-736-8031 x56442',
+        website: 'hildegard.org',
+        company: {
+            name: 'Romaguera-Crona',
+            catchPhrase: 'Multi-layered client-server neural-net',
+            bs: 'harness real-time e-markets',
+        },
+    },
+    {
+        id: 2,
+        name: 'Ervin Howell',
+        username: 'Antonette',
+        email: 'Shanna@melissa.tv',
+        address: {
+            street: 'Victor Plains',
+            suite: 'Suite 879',
+            city: 'Wisokyburgh',
+            zipcode: '90566-7771',
+            geo: {
+                lat: '-43.9509',
+                lng: '-34.4618',
+            },
+        },
+        phone: '010-692-6593 x09125',
+        website: 'anastasia.net',
+        company: {
+            name: 'Deckow-Crist',
+            catchPhrase: 'Proactive didactic contingency',
+            bs: 'synergize scalable supply-chains',
+        },
+    },
+];
+console.log(getUserProperty(usersArray, 'email'));
+console.log(getUserProperty(usersArray, 'username'));
